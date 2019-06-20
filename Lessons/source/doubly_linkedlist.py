93c060e1edc4df9825a2615dd68be924282ebc95
erica2605@live.com <erica2605@live.com>
2019-06-19 18:56:15 -0700

    progress

--- Lessons/source/doubly_linkedlist.py
+++ Lessons/source/doubly_linkedlist.py
@@ -1,11 +1,30 @@
-class Doubly_LinkedList(object):
-    def find():
-        pass
+class Node(object):
 
-    def append():
-        pass
+    def __init__(self, data):
+        """Initialize this node with the given data."""
+        self.data = data
+        self.next = None
+        self.prev = None
+
+    def __repr__(self):
+        """Return a string representation of this node."""
+        return 'Node({!r})'.format(self.data)
+
+class Doubly_LinkedList(object):
+
+    def append(item):
+        new_node = Node(item)
+        self.tail.next = new_node
+        new_node.prev = self.tail
+        self.tail = new_node
         
     def prepend():
+        new_node = Node(item)
+        self.head.prev = new_node
+        new_node.next = self.head
+        self.head = new_node
+
+    def find():
         pass
 
     def delete():
