2fd404db8453a9ef78feece307ccc16623816a1a
erica2605@live.com <erica2605@live.com>
2019-06-20 17:55:42 -0700

    added complexity annotations

--- Lessons/source/queue.py
+++ Lessons/source/queue.py
@@ -22,24 +22,20 @@ class LinkedQueue(object):
 
     def is_empty(self):
         """Return True if this queue is empty, or False otherwise."""
-        # TODO: Check if empty
         return self.list.is_empty()
 
     def length(self):
         """Return the number of items in this queue."""
-        # TODO: Count number of items
         return self.list.length()
 
     def enqueue(self, item):
         """Insert the given item at the back of this queue.
-        Running time: O(???) – Why? [TODO]"""
-        # TODO: Insert given item
+        Running time: O(1) – because we always add to the same spot and only modify one value"""
         self.list.append(item)
 
     def front(self):
         """Return the item at the front of this queue without removing it,
         or None if this queue is empty."""
-        # TODO: Return front item, if any
         if self.is_empty():
             return None
         else:
@@ -48,8 +44,7 @@ class LinkedQueue(object):
     def dequeue(self):
         """Remove and return the item at the front of this queue,
         or raise ValueError if this queue is empty.
-        Running time: O(???) – Why? [TODO]"""
-        # TODO: Remove and return front item, if any
+        Running time: O(1) we always add to the same spot and only modify one value"""
         if self.is_empty():
             raise ValueError("list is empty!")
         else:
@@ -76,23 +71,19 @@ class ArrayQueue(object):
 
     def is_empty(self):
         """Return True if this queue is empty, or False otherwise."""
-        # TODO: Check if empty
         return len(self.list) == 0
     def length(self):
         """Return the number of items in this queue."""
-        # TODO: Count number of items
         return len(self.list)
 
     def enqueue(self, item):
         """Insert the given item at the back of this queue.
-        Running time: O(???) – Why? [TODO]"""
-        # TODO: Insert given item
+        Running time: O(1) because we always add to the same spot and only modify one value"""
         self.list.append(item) 
 
     def front(self):
         """Return the item at the front of this queue without removing it,
         or None if this queue is empty."""
-        # TODO: Return front item, if any
         if self.is_empty():
             return None
         else:
@@ -101,8 +92,7 @@ class ArrayQueue(object):
     def dequeue(self):
         """Remove and return the item at the front of this queue,
         or raise ValueError if this queue is empty.
-        Running time: O(???) – Why? [TODO]"""
-        # TODO: Remove and return front item, if any
+        Running time: O(n) worst case because you have to change the indexes of other items in the list"""
         if self.is_empty():
             raise ValueError("queue is empty!")
         else:
@@ -112,4 +102,3 @@ class ArrayQueue(object):
 # Implement LinkedQueue and ArrayQueue above, then change the assignment below
 # to use each of your Queue implementations to verify they each pass all tests
 Queue = ArrayQueue
-# Queue = ArrayQueue
